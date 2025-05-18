5d983e9f9950846ba0900a864b931421d03fb28b
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-17 21:11:52 -0500

    buscando pelicula por nombre de autor

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
+//Modulos necesarios
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const fs = require('fs');
 const app = express();
 const PORT = 3000;
-
+// Conexión a la base de datos 'imdb.db'
 const db = new sqlite3.Database('./imdb.db', (err) => {
   if (err) {
     console.error('Error al conectar a la base de datos:', err.message);
   } else {
     console.log('Conectado a imdb.db');
   }
+});
+// muestra a index.html
+app.get('/', (req, res) => {
+  fs.readFile('index.html', 'utf-8', (err, data) => {
+    if (err) return res.status(500).send('Error al cargar el HTML');
+    res.send(data);
+  });
+});
+
+// Buscar películas por nombre del actor
+app.get('/peliculas', (req, res) => {
+  const nombreActor = req.query.nombre;    // Extrae el parámetro 'nombre' de la URL
+
+  if (!nombreActor) {
+    return res.status(400).json({ error: 'Falta el parámetro "nombre"' });
+  }
+
+  const query = `
+  `;
+
+
 });
 
